ac9b56688856c87b554d0494e9550014364442eb
Caccinolo Benoit <benoit.caccinolo@gmail.com>
2011-08-30 16:14:53 +0200

    addition of some options

--- bin/geek_typist.rb
+++ bin/geek_typist.rb
@@ -1,14 +1,46 @@
 #gem install highline
 
 require "rubygems"
+require 'optparse'
 require "highline/system_extensions"
 include HighLine::SystemExtensions
 
-str_test = "{13-_87}"
 
-#debugger
+
+
+
+options = {}
+OptionParser.new do |opts|
+opts.banner = "Usage: geek_typist [options]"
+
+opts.on("-v", "--version", "Version") do |v|
+  options[:version] = 1
+end
+
+opts.on("-t text", "--text text", "launch the system with the given text ") do |v|
+  options[:use_text] = v 
+end
+
+opts.on("-n NUM", "--n-times NUM", "repeat the text NUM times") do |v|
+  options[:repeat] = v 
+end
+
+end.parse!
+
+if options[:use_text]
+  str_test = options[:use_text]
+else
+  str_test = "{13-_87}"
+end
+
+if options[:repeat]
+  n_times = options[:repeat].to_i
+else
+  n_times = 1
+end
+
 failures = 0
-str_test.split("").each do |s| 
+(str_test * n_times).split("").each do |s| 
 
   c = 0
   while (c != s[0]) do 
